440c2d906b8df415900b8ddd834a9d0f3fcfe694
Eric <zhangl127@myunitec.ac.nz>
2018-03-25 22:15:23 +1300

    Set up interface

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import TodoInput from './components/todoInput';
+import TodoItem from './components/todoItem';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      todos: [
+        {id: 0, text: "Make dinner"},
+        {id: 1, text: "Shop for food"},
+        {id: 2, text: "Learn react js"}
+      ],
+      nextId: 3
+    }
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+
+addTodo(todoText){
+  console.log("Todo added: ", todoText);
+}
+
+removeTodo(id){
+  console.log("Removed: ", id);
+}
+
   render() {
     return (
       <div className="App">
         <div className="todo-wrapper">
-        Hello
-
+        <Header />
+        <TodoInput todoText="" addTodo={this.addTodo}/>
+        <ul>
+          {
+            this.state.todos.map(
+              (todo) => {
+                return <TodoItem todo={todo} key={todo.id} id={todo.id} removeTodo={this.removeTodo}/>
+              }
+            )
+          }
+        </ul>
         </div>
-        // <header className="App-header">
-        //   <img src={logo} className="App-logo" alt="logo" />
-        //   <h1 className="App-title">Welcome to React</h1>
-        // </header>
-        // <p className="App-intro">
-        //   To get started, edit <code>src/App.js</code> and save to reload.
-        // </p>
       </div>
     );
   }
